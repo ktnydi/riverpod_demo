a3ac30628cc041a183ead8252b34bd5d56f286de
Yudai Kitano <kitano.prog@gmail.com>
2020-10-26 23:59:07 +0900

    追加: ユニットテスト＆ウィジェットテストのデモ

---
+++ test/my_widget/my_widget_page_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:riverpod_app/presentation/my_widget/my_widget_page.dart';
+
+void main() {
+  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    await tester.pumpWidget(MyWidget(
+      title: 'T',
+      message: 'M',
+    ));
+
+    final titleFinder = find.text('T');
+    final messageFinder = find.text('M');
+
+    expect(titleFinder, findsOneWidget);
+    expect(messageFinder, findsOneWidget);
+  });
+}
